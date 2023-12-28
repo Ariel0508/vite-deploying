31aa6fb19c503fc6a361c482d0f93344a62275c4
Ariel <sonyt605@gmail.com>
2023-12-28 23:37:09 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:Ariel0508/Ariel0508.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Ariel0508/vite-deploying.git main:gh-pages
+git push -f git@github.com:Ariel0508/vite-deploying.git main:gh-pages
 
 cd -
