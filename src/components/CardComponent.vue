29834b2f09a909adafd82312c85155d3d083ec64
Ariel <sonyt605@gmail.com>
2023-12-31 02:37:35 +0800

    cart

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,29 @@
+<template>
+      <div class="card">
+        <img :src="product.imageUrl" class="card-img-top" :alt="product.title" />
+        <div class="card-body">
+          <h6 class="card-title">
+            {{ product.title }}
+            <span class="float-end">$ {{ product.price }}</span>
+          </h6>
+          <a href="#" class="btn btn-outline-primary w-100" @click.prevent="addToCart(product.id, num)">
+            加入購物車
+          </a>
+        </div>
+      </div>
+</template>
+<script>
+import cartStore from "../stores/cartStore.js";
+import  { mapActions } from "pinia"
+export default {
+    props: ['product'],
+    data(){
+        return{
+            num:1
+        }
+    },
+    methods: {
+        ...mapActions(cartStore, ["addToCart"])
+      },
+  };
+</script>
---
+++ src/components/ProductComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row row-cols-3 my-4 g-4">
+      <div class="col" v-for="product in sortProducts" :key="product.id">
+        <CardComponent :product="product"></CardComponent>
+      </div>
+    </div>
+</template>
+
+<script>
+import ProductStore from '../stores/productStore.js'
+import CartStore from '../stores/cartStore.js'
+import CardComponent from './CardComponent.vue'
+import { mapState, mapActions } from 'pinia'
+
+export default {
+  computed: {
+    ...mapState(ProductStore, ['sortProducts'])
+  },
+  methods: {
+    ...mapActions(CartStore, ['addToCart'])
+  },
+  components: {
+    CardComponent
+  }
+}
+</script>
